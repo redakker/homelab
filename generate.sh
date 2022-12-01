67378858bd2f7d237cdfce44ee08b452f79dce04
akos.voros <akos.voros@enghouse.com>
2022-12-01 10:52:07 +0100

    Generator change

--- generate.sh
+++ generate.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo '#!/bin/bash' > start.sh
-echo 'docker-compose \' >> start.sh
+#echo 'docker-compose \' >> start.sh
 
 for f in $(pwd)/**/docker-compose.y*ml ; do
 
-    echo '-f '$f' \' >> start.sh
+    echo 'docker-compose -f '$f' up -d --build --remove-orphans --always-recreate-deps' >> start.sh
 
 done;
 
-echo ' up --build --remove-orphans --always-recreate-deps' >> start.sh
+#echo ' up --build --remove-orphans --always-recreate-deps' >> start.sh
 
 chmod +x start.sh
